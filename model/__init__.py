8a4f219533d15a6476a25fa14d96a0af3affd950
Akhileswar <115480942+Akhil351@users.noreply.github.com>
2026-01-28 19:00:28 +0530

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,3 @@
-from .schemas import llm_schema1, llm_schema2
+from .schemas import llm_schema1, llm_schema2 , movie_review_schema
 
-__all__ = ["llm_schema1", "llm_schema2"]
+__all__ = ["llm_schema1", "llm_schema2", "movie_review_schema"]
